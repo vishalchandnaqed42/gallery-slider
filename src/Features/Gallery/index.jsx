a150bea34ce36eb6cbfb3888807d31fe7f78046d
qed42 <qed42@Vishal-MacNook.local>
2021-08-23 14:46:43 +0530

    Comments added.

--- src/Features/Gallery/index.jsx
+++ src/Features/Gallery/index.jsx
@@ -18,9 +18,11 @@ const Gallery = ({ data = [], maxWidth = 800, maxHeight = 400, visibleItems = 1,
 
   const styles = {
     gallery: {
+      // In case of mobile, limit the height to 30%
       maxWidth, maxHeight: mobileMatchMedia.matches ? '30%' : maxHeight,
     },
     galleryItem: {
+      // deriveCSSNumber, a small utility, to convert it into CSS format wrt its value.
       minWidth: `calc(${deriveCSSNumber(width)}/${visibleItems} - 2 * ${deriveCSSNumber(spacing)})`,
       minHeight: '100%',
       padding: `0 ${deriveCSSNumber(spacing)}`,
@@ -33,13 +35,14 @@ const Gallery = ({ data = [], maxWidth = 800, maxHeight = 400, visibleItems = 1,
       try {
         const scroll = scrollTo(galleryEl);
         const { scrollLeft, clientWidth, scrollWidth } = galleryEl;
-        if (scrollWidth - (scrollLeft + clientWidth) <= (!isNaN(spacing) ? spacing : 10)) {
+        if (scrollWidth - (scrollLeft + clientWidth) <= (!isNaN(spacing) ? spacing : 10))
+          // A small wrapped utility to handle scrolling.
           scroll({ left: 0, behaviour: 'smooth' });
-        }
-        else {
+        else
           scroll({ left: scrollLeft + clientWidth, behaviour: 'smooth' });
-        }
+
         const leftSlides = data.length - slideStartIndex - visibleItems;
+        // Set the active indexes on the elements
         if (leftSlides === 0) setSlideStartIndex(0);
         else setSlideStartIndex(leftSlides >= visibleItems ? slideStartIndex + visibleItems : slideStartIndex + leftSlides);
       } catch (error) {
@@ -56,12 +59,12 @@ const Gallery = ({ data = [], maxWidth = 800, maxHeight = 400, visibleItems = 1,
       try {
         const scroll = scrollTo(galleryEl);
         const { scrollLeft, clientWidth, scrollWidth } = galleryEl;
-        if (scrollLeft <= (!isNaN(spacing) ? spacing : 10)) {
+        if (scrollLeft <= (!isNaN(spacing) ? spacing : 10))
           scroll({ left: scrollWidth - clientWidth, behaviour: 'smooth' });
-        }
-        else {
+        else
           scroll({ left: scrollLeft - clientWidth, behaviour: 'smooth' });
-        }
+
+        // Set the active indexes on the elements
         if (slideStartIndex === 0) setSlideStartIndex(data.length - 1 - visibleItems);
         else setSlideStartIndex(slideStartIndex >= visibleItems ? slideStartIndex - visibleItems : 0);
       } catch (error) {
@@ -74,6 +77,7 @@ const Gallery = ({ data = [], maxWidth = 800, maxHeight = 400, visibleItems = 1,
 
   useEffect(() => {
     if (!auto) return;
+
     const timer = setInterval(() => {
       onNext();
     }, autoInterval);
@@ -85,18 +89,19 @@ const Gallery = ({ data = [], maxWidth = 800, maxHeight = 400, visibleItems = 1,
   useEffect(() => {
     const galleryEl = galleryRef.current
     galleryEl.scrollLeft = 0;
-    galleryEl.style.scrollBehavior = "smooth";
-    setWidth(galleryEl.clientWidth);
+    galleryEl.style.scrollBehavior = "smooth"; // smooth behaviour polyfill for Safari
+    setWidth(galleryEl.clientWidth); // Get the actual width in case if it is set in relative number e.g 40%
   }, []);
 
 
+  const isActiveIndex = (index) => (index >= slideStartIndex) && (index < (slideStartIndex + visibleItems));
   return (
     <Container id="gallery" className="gallery" style={styles.gallery}>
       <Container className="gallery-scroller" ref={galleryRef}>
         {data.length === 0 && NO_DATA}
         {data.map((item, index) => {
           const { id } = item
-          return <GalleryItem key={id} data={item} style={styles.galleryItem} title={`Gallery item - ${id}`} alt={`Gallery item - ${id}`} data-active={(index >= slideStartIndex) && (index < (slideStartIndex + visibleItems))}></GalleryItem>
+          return <GalleryItem key={id} data={item} style={styles.galleryItem} title={`Gallery item - ${id}`} alt={`Gallery item - ${id}`} data-active={isActiveIndex(index)}></GalleryItem>
         })}
       </Container>
       <GalleryNavigationButton onPrev={onPrev} onNext={onNext} />
